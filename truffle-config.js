a56b77006e16f409be078a1fd6d38ce6160a631a
Pratchaya Saenphoi <B6311636@g.sut.ac.th>
2023-06-13 11:36:53 +0700

    ok state 1

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-// const keys = require("./keys.json");
+const keys = require("./keys.json");
 
 module.exports = {
   contracts_build_directory: "./public/contracts",
@@ -10,22 +10,22 @@ module.exports = {
       port: 7545,
       network_id: "*",
     },
-    // sepolia: {
-    //   provider: () => 
-    //     new HDWalletProvider(
-    //       keys.MNEMONIC,
-    //       keys.INFURA_API_KEY
-    //     ),
-    //   network_id: 11155111,
-    //   gas:  5500000,
-    //      gasPrice: 20000000000,
-    //     confirmations: 2,
-    //     timeoutBlocks: 200
-    // }
-    },
-    compilers: {
-      solc: {
-        version: "0.8.13",
-      }
-    },
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider(
+          keys.MNEMONIC,
+          keys.INFURA_API_KEY
+        ),
+      network_id: 11155111,
+      gas: 5500000,
+      gasPrice: 20000000000,
+      confirmations: 2,
+      timeoutBlocks: 200
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.13",
+    }
+  },
 };
